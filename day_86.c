024a53460b40c50b7a33298158a3261e051e5d8e
Ansh <anshmonga0203@gmail.com>
2026-05-05 23:02:05 +0530

    Create day_86.c

---
+++ day_86.c
@@ -0,0 +1,66 @@
+/*Problem: Find the integer square root of a given non-negative integer using Binary Search.
+The integer square root of a number is defined as the greatest integer whose square is less than or equal to the given number.
+
+Input:
+- First line: an integer n
+
+Output:
+- Print the integer square root of n
+
+Examples:
+Input:
+64
+
+Output:
+8
+
+Input:
+20
+
+Output:
+4
+
+Input:
+1
+
+Output:
+1
+
+Explanation:
+Binary Search is applied on the range 0 to n. At each step, the middle value is squared and compared with n. If mid * mid equals n, mid is the square root. If mid * mid is less than n, search continues in the right half; otherwise, in the left half. 
+The final answer is the largest integer whose square does not exceed n.*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    // Taking input from user
+    printf("Enter a non-negative number: ");
+    scanf("%d", &n);
+
+    int left = 0, right = n;
+    int ans = 0;
+
+    // Binary Search
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (mid * mid == n) {
+            ans = mid;
+            break;
+        }
+        else if (mid * mid < n) {
+            ans = mid;        // possible answer
+            left = mid + 1;   // search right
+        }
+        else {
+            right = mid - 1;  // search left
+        }
+    }
+
+    // Output result
+    printf("Integer square root of %d is %d\n", n, ans);
+
+    return 0;
+}
